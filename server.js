2d3517589b906f05b0f7caaa5a9941beb4fa2b43
Glitch (edapi) <none>
2018-04-24 22:31:41 +0000

    😬⛄️ Checkpoint ./server.js:136798/1007

--- server.js
+++ server.js
@@ -6,13 +6,26 @@ var app = express()
 var Discogs = require('disconnect').Client
 var db = new Discogs('ExplorerDiscogsApi/0.0.0').database()
 
+// init redis cache
+const redis = require('redis')
+var {REDIS_URL} = process.env
+if (REDIS_URL === undefined) {  
+  console.error("Please set the COMPOSE_REDIS_URL environment variable");
+  process.exit(1);
+}
+let redisClient = redis.createClient(REDIS_URL, {
+  tls: {
+    servername: new URL(REDIS_URL).hostname
+  }
+})
+
 // index route
 app.get('/', function (request, response) {
   response.send({msg: 'discogs proxy api'})
 })
 
 app.get('/releases/:id', async (req, res) => {
-  db.getRelease(req.params.id, function(err, data){
+  db.getRelease(req.params.id, function(err, data) {
     console.log(data)
     res.send(data)
   })
