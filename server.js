df5edea8568cf67e90e38f9c68369f83fdd7acb8
Glitch (edapi) <none>
2018-04-24 23:03:18 +0000

    🐋💀 Checkpoint ./server.js:136798/8830

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ var db = new Discogs('ExplorerDiscogsApi/0.0.0').database()
 // init redis cache
 const Redis = require('ioredis')
 
-var {REDIS_URL} = process.env
-if (REDIS_URL === undefined) {  
-  console.error("Please set the COMPOSE_REDIS_URL environment variable");
-  process.exit(1);
+var { REDIS_URL } = process.env
+if (REDIS_URL === undefined) {
+	console.error('Please set the COMPOSE_REDIS_URL environment variable')
+	process.exit(1)
 }
 
 const redis = new Redis(REDIS_URL)
@@ -31,22 +31,32 @@ const redis = new Redis(REDIS_URL)
 // }
 
 // index route
-app.get('/', function (request, response) {
-  response.send({msg: 'discogs proxy api'})
+app.get('/', function(request, response) {
+	response.send({ msg: 'discogs proxy api' })
 })
 
-app.get('/releases/:id', async (req, res) => {
-  const id = req.params.id
-  const key = `releases/${id}`
-  redis.get(key, (err, result) => {
-    if (result) res.send(result) 
-  })
-  db.getRelease(id, function(err, data) {
-    redis.set(key, 3600, 'EX', data)
-    res.send(data)
-  })
+app.get('/releases/:id', (req, res) => {
+	const id = req.params.id
+	const key = `releases/${id}`
+  
+  console.log({id, key})
+
+	return redis
+		.get(key)
+		.then(result => {
+			console.log(`found cache`)
+			res.send(result)
+		})
+		.catch(() => {
+			console.log('no cache')
+			db.getRelease(id, function(err, data) {
+				redis.set(key, 3600, 'EX', data)
+				console.log('setting cache', key)
+				res.send(data)
+			})
+		})
 })
 
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port)
+var listener = app.listen(process.env.PORT, function() {
+	console.log('Your app is listening on port ' + listener.address().port)
 })
