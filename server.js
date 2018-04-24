d1bdcc62556af86254fd6f5c733903ed9898cc15
Glitch (edapi) <none>
2018-04-24 22:52:39 +0000

    🔮🐀 Checkpoint ./server.js:136798/993 ./package.json:136798/1378

--- server.js
+++ server.js
@@ -7,14 +7,15 @@ var Discogs = require('disconnect').Client
 var db = new Discogs('ExplorerDiscogsApi/0.0.0').database()
 
 // init redis cache
-const redis = require('redis')
+const Redis = require('ioredis')
+
 var {REDIS_URL} = process.env
 if (REDIS_URL === undefined) {  
   console.error("Please set the COMPOSE_REDIS_URL environment variable");
   process.exit(1);
 }
-let redisClient = redis.createClient(REDIS_URL)
 
+const redis = new Redis(REDIS_URL)
 // express cache middleware function
 // function cache(req, res, next) {
 //     const id = req.params.id;
@@ -35,15 +36,15 @@ app.get('/', function (request, response) {
 })
 
 app.get('/releases/:id', async (req, res) => {
-  res.send(req.params)
-  // const key = `release_${req.params.id}`
-  // redisClient.get(key, (err, result) => {
-  //   if (err)
-  // })
-  // db.getRelease(req.params.id, function(err, data) {
-  //   console.log(data)
-  //   res.send(data)
-  // })
+  const id = req.params.id
+  const key = `releases/${id}`
+  redis.get(key, (err, result) => {
+    if (result) res.send(result) 
+  })
+  db.getRelease(id, function(err, data) {
+    redis.set(key, 3600, 'EX', data)
+    res.send(data)
+  })
 })
 
 var listener = app.listen(process.env.PORT, function () {
