ef44fc9c87818dfec4fe79352bfedeac468ac613
Glitch (edapi) <none>
2018-04-24 23:45:56 +0000

    ✂️🌭 Checkpoint ./server.js:136798/4384 ./README.md:136798/786

--- server.js
+++ server.js
@@ -8,50 +8,40 @@ var db = new Discogs('ExplorerDiscogsApi/0.0.0').database()
 
 // init redis cache
 const Redis = require('ioredis')
-const {URL} = require('url')
-var { REDIS_URL } = process.env
+const { URL } = require('url')
+const { REDIS_URL } = process.env
 if (REDIS_URL === undefined) {
 	console.error('Please set the REDIS_URL environment variable')
 	process.exit(1)
 }
 const redis = new Redis(REDIS_URL, {
-  tls: { servername: new URL(REDIS_URL).hostname }
+	tls: { servername: new URL(REDIS_URL).hostname }
 })
 
-redis.on('error', (err) => {
-  console.log(err)
-})
-
-// index route
 app.get('/', function(request, response) {
 	response.send({ msg: 'discogs proxy api' })
 })
 
-app.get('/releases/:id', (req, res) => {
+app.get('/releases/:id', async (req, res) => {
 	const id = req.params.id
 	const key = `releases/${id}`
-  
-  console.log({id, key})
-  
-  redis.get(key, function (err, result) {
-    if (err) {
-      console.log('no cache')
-      console.error(err)
-      db.getRelease(id, (err, data) => {
-        console.log('setting cache', key)
-        redis.set(key, JSON.stringify(data), 'EX', 60)
-        res.send(data)
-      })
-    } else if (result) {
-      console.log('found cache')
-      console.log(result)
-      res.send(JSON.parse(result))
-    } else {
-      res.send({error: 'something went wrong…'})
-    }
-  })
+
+	console.log({ id, key })
+
+	let cache = await redis.get(key)
+	if (cache) {
+		console.log('found cache')
+		res.send(JSON.parse(cache))
+	} else {
+		console.log('no cache')
+		db.getRelease(id, (err, data) => {
+			console.log('setting cache', key)
+			redis.set(key, JSON.stringify(data), 'EX', 60)
+			res.send(data)
+		})
+	}
 })
 
 var listener = app.listen(process.env.PORT, function() {
-	console.log('Your app is listening on port ' + listener.address().port)
+	console.log(`Your app is listening on port ${listener.address().port}`)
 })
