74b1a246be6a86536637bcc058eefc6e8d883e8f
Glitch (edapi) <none>
2018-04-25 12:07:37 +0000

    🍖💶 Checkpoint ./server.js:136798/260

--- server.js
+++ server.js
@@ -29,35 +29,31 @@ redis.on('connect', () => console.log('connected to redis'))
 redis.on('end', () => console.log('disconnected from redis'))
 
 
-
 // Express middleware for caching
 async function cache(req, res, next) {
-  res.locals.key = req.originalUrl
+  const key = req.originalUrl
   
   // if cache exists, return it
-	let cache = await redis.get(res.locals.key)
+	let cache = await redis.get(key)
   if (cache) {
     console.log('using cache')
     res.send(cache)
     return
   }
   
-  // otherwise overwrite "res.send" so we can save cache
-  // before sending the response
+  // otherwise overwrite "res.send" so we can save cache before sending the response
   res.sendResponse = res.send
   res.send = (body) => {
-    console.log('wut saving and replying cache')
-    redis.set(res.locals.key, body, 'EX', CACHE_SECONDS)
+    console.log('no cache found. creating cache')
+    redis.set(key, body, 'EX', CACHE_SECONDS)
     res.sendResponse(body)
   }
   
   next()
 }
 
-app.use(cache)
-
 // express routes
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.send({
     msg: 'Proxy API for Discogs',
     'test release': `https://${req.headers.host}/releases/6980600`,
@@ -66,21 +62,20 @@ app.get('/', (req, res, next) => {
   })
 })
 
-app.get('/releases/:id', async (req, res) => {
+app.get('/releases/:id', cache, async (req, res) => {
   const data = await db.getRelease(req.params.id)
   res.send(data)
 })
 
-app.get('/labels/:id', async (req, res) => {
+app.get('/labels/:id', cache, async (req, res) => {
   const data = await db.getLabel(req.params.id)
   res.send(data)
 })
 
-app.get('/masters/:id', async (req, res) => {
+app.get('/masters/:id', cache, async (req, res) => {
   const data = await db.getMaster(req.params.id)
   res.send(data)
 })
-
 
 const listener = app.listen(process.env.PORT, function() {
 	console.log(`Your app is listening on port ${listener.address().port}`)
